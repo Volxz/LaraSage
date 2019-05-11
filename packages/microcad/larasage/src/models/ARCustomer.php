28699e10ea9ef9baa2516552ca340e5d9fa2c1a6
Volxz <ethan.gallant@gmail.com>
2019-05-11 14:25:56 -0400

    Add explicit namespace for ARCustomer

--- packages/microcad/larasage/src/models/ARCustomer.php
+++ packages/microcad/larasage/src/models/ARCustomer.php
@@ -1,9 +1,11 @@
 <?php
 
+namespace Microcad\LaraSage\Models;
+
 use Microcad\LaraSage\SageCRUD;
 
-class ARCustomer extends SageCRUD {
-    protected $endpoint = "/AR/ARCustomers";
-
+class ARCustomer extends SageCRUD
+{
+    static protected $endpoint = "/AR/ARCustomers";
 
 }
